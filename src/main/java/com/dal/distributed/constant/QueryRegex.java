89113feadf188287c3486ee5973d82e79f979ccf
Chanpreet Singh <ch835893@dal.ca>
2022-04-07 16:54:42 -0300

    Added entity in QueryValidator and regex updations

--- src/main/java/com/dal/distributed/constant/QueryRegex.java
+++ src/main/java/com/dal/distributed/constant/QueryRegex.java
@@ -6,8 +6,10 @@ public class QueryRegex {
     public static final Pattern createDatabase = Pattern.compile("create\\s+database\\s+(\\w+)\\;", Pattern.CASE_INSENSITIVE);
     public static final Pattern useDatabase = Pattern.compile("USE\\s+(\\w+)\\;", Pattern.CASE_INSENSITIVE);
     public static final Pattern createTable = Pattern.compile("CREATE\\s+TABLE\\s+(\\w+)\\s*\\((.+)\\)\\;", Pattern.CASE_INSENSITIVE);
-    public static final Pattern updateTable = Pattern.compile("UPDATE\\s+(\\w+)\\s+SET\\s+(\\w+\\=(\\w|\\'\\'|\\'\\w+\\'))\\s+(WHERE\\s+(\\w+\\s*(\\=|\\<|\\>|LIKE|\\<\\=|\\>\\=)\\s*(\\'\\w*\\'|\\w+)))?\\;", Pattern.CASE_INSENSITIVE);
-    public static final Pattern deleteDataInTable = Pattern.compile("DELETE\\s+FROM\\s+(\\w+)\\s+WHERE\\s+(\\w+\\s*(\\=|\\<|\\>|LIKE|\\<\\=|\\>\\=)\\s*(\\'\\w*\\'|\\w+))\\;", Pattern.CASE_INSENSITIVE);
-    public static final Pattern insertDataIntoTable = Pattern.compile("insert\\s+into\\s+(\\w+)((.+))?\\s+values\\s*\\((.+)\\);", Pattern.CASE_INSENSITIVE);
-    public static final Pattern selectDataFromTable = Pattern.compile("select\\s+((\\*)|(\\w+)|(\\w+\\,\\s*)+\\w+)\\s+from\\s+\\w+\\s*(where\\s+(\\w+)\\s*(\\=|LIKE|IN|\\<|\\>|\\!\\=|\\<\\=|\\>\\=)\\s*(\\d+|\\'\\w+\\'))?\\;", Pattern.CASE_INSENSITIVE);
+    public static final Pattern updateTable = Pattern.compile("UPDATE\\s+(\\w+)\\s+SET\\s+(\\w+\\=(\\w|\\'\\'|\\'\\w+\\'))\\s*(WHERE\\s+(\\w+\\s*(\\=|\\<|\\>|\\<\\=|\\>\\=)\\s*(\\'\\w*\\'|\\w+)))?\\;", Pattern.CASE_INSENSITIVE);
+    public static final Pattern deleteDataInTable = Pattern.compile("DELETE\\s+FROM\\s+(\\w+)\\s+WHERE\\s+(\\w+\\s*(\\=|\\<|\\>|\\<\\=|\\>\\=)\\s*(\\'\\w*\\'|\\w+))\\;\\s*$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern insertDataIntoTable = Pattern.compile("insert\\s+into\\s+(\\w+)((.+))?\\s+values\\s*\\((.+)\\);\\s*$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern selectDataFromTable = Pattern.compile("select\\s+((\\*)|(\\w+)|(\\w+\\,\\s*)+\\w+)\\s+from\\s+(\\w+)\\s*(where\\s+(\\w+)\\s*(\\=|\\<|\\>|\\!\\=|\\<\\=|\\>\\=)\\s*(\\d+|\\'\\w+\\'))?\\;$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern startTransaction = Pattern.compile("^start\\s+transaction;\\s*$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern endTransaction = Pattern.compile("^end\\s+transaction;\\s*$", Pattern.CASE_INSENSITIVE);
 }
--- src/main/java/com/dal/distributed/queryImpl/QueryValidator.java
+++ src/main/java/com/dal/distributed/queryImpl/QueryValidator.java
@@ -16,42 +16,63 @@ public class QueryValidator {
         if (matcher.find()) {
             result.replace("isValidate", true);
             result.put("queryType", QueryTypes.CREATE_DATABASE);
+            result.put("entity", matcher.group(1));
         }
 
         matcher = QueryRegex.useDatabase.matcher(query);
         if (matcher.find()) {
             result.replace("isValidate", true);
             result.put("queryType", QueryTypes.USE);
+            result.put("entity", matcher.group(1));
         }
 
         matcher = QueryRegex.createTable.matcher(query);
         if (matcher.find()) {
             result.replace("isValidate", true);
             result.put("queryType", QueryTypes.CREATE_TABLE);
+            result.put("entity", matcher.group(1));
         }
 
         matcher = QueryRegex.updateTable.matcher(query);
         if (matcher.find()) {
             result.replace("isValidate", true);
             result.put("queryType", QueryTypes.UPDATE);
+            result.put("entity", matcher.group(1));
         }
 
         matcher = QueryRegex.deleteDataInTable.matcher(query);
         if (matcher.find()) {
             result.replace("isValidate", true);
             result.put("queryType", QueryTypes.DELETE);
+            result.put("entity", matcher.group(1));
         }
 
         matcher = QueryRegex.insertDataIntoTable.matcher(query);
         if (matcher.find()) {
             result.replace("isValidate", true);
             result.put("queryType", QueryTypes.INSERT);
+            result.put("entity", matcher.group(1));
         }
 
         matcher = QueryRegex.selectDataFromTable.matcher(query);
         if (matcher.find()) {
             result.replace("isValidate", true);
             result.put("queryType", QueryTypes.SELECT);
+            result.put("entity", matcher.group(5));
+        }
+
+        matcher = QueryRegex.startTransaction.matcher(query);
+        if (matcher.find()) {
+            result.replace("isValidate", true);
+            result.put("queryType", QueryTypes.START_TRANSACTION);
+            result.put("entity", null);
+        }
+
+        matcher = QueryRegex.endTransaction.matcher(query);
+        if (matcher.find()) {
+            result.replace("isValidate", true);
+            result.put("queryType", QueryTypes.END_TRANSACTION);
+            result.put("entity", null);
         }
         return result;
     }
