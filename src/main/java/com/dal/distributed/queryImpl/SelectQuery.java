1410ea4f6f5bbf1dfd1582c0523788b65c8da25a
Sanika Tamhankar <sn295037@dal.ca>
2022-04-08 23:37:09 -0300

    Transaction processing changes

--- src/main/java/com/dal/distributed/queryImpl/SelectQuery.java
+++ src/main/java/com/dal/distributed/queryImpl/SelectQuery.java
@@ -3,10 +3,14 @@ package com.dal.distributed.queryImpl;
 import com.dal.distributed.constant.DataConstants;
 import com.dal.distributed.constant.MiscConstants;
 import com.dal.distributed.constant.QueryRegex;
+import com.dal.distributed.constant.QueryTypes;
 import com.dal.distributed.constant.RelationalOperators;
 import com.dal.distributed.utils.DataUtils;
 import com.dal.distributed.utils.FileOperations;
+import com.dal.distributed.utils.Results;
 import com.dal.distributed.logger.Logger;
+import com.dal.distributed.main.Main;
+import com.dal.distributed.queryImpl.model.OperationStatus;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,9 +18,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 
+import javax.naming.spi.DirStateFactory.Result;
+
 public class SelectQuery {
     Logger logger = Logger.instance();
-    public void execute(String query) throws Exception {
+    public OperationStatus execute(String query) throws Exception {
+        OperationStatus operationStatus=null;
         List<Map> resultList = new ArrayList();
         List<List<Object>> queryResult=new ArrayList<>();
         Map resultDict;
@@ -25,8 +32,9 @@ public class SelectQuery {
             String tableName = matcher.group(5);
 //        Query to test: select * from Persons2;
 //        Uncomment the line when not testing
-//            ArrayList fileContent = FileOperations.readPsvFileForQueryOps(DataConstants.DATABASES_FOLDER_LOCATION + Main.databaseName + "/" + table + DataConstants.FILE_FORMAT);
-            ArrayList fileContent = FileOperations.readPsvFileForQueryOps(DataConstants.DATABASES_FOLDER_LOCATION + "dbdbdb/" + tableName + DataConstants.FILE_FORMAT);
+            String filePath=DataConstants.DATABASES_FOLDER_LOCATION + Main.databaseName + "/" + tableName + DataConstants.FILE_FORMAT;
+          ArrayList fileContent = FileOperations.readPsvFileForQueryOps(filePath);
+            //ArrayList fileContent = FileOperations.readPsvFileForQueryOps(DataConstants.DATABASES_FOLDER_LOCATION + "dbdbdb/" + tableName + DataConstants.FILE_FORMAT);
             String projections = matcher.group(1);
             if(projections == null)
                 logger.info("Oops.. looks like you did not add projections! Please try again!");
@@ -134,13 +142,26 @@ public class SelectQuery {
                         resultList.add(dataDict);
                     }
                 }
+                List<Object> mapToList=new ArrayList<>();
                 resultList = this.filterProjectionsForOutput(resultList, projectionList);
-                for(Map m:resultList){
-                    //for(Map.Entry<String,String> pair : m.entrySet())
-                    
+                int c=0;
+                for(Map<String,String> m:resultList){
+                    for(Map.Entry<String,String> mp : m.entrySet())
+                    {
+                        if(c==0)
+                        mapToList.add(mp.getKey());
+                        else
+                        mapToList.add(mp.getValue());
+                        c=c+1;
+                    }
+                    queryResult.add(mapToList);
                 }
+                operationStatus=new OperationStatus(true, queryResult, query, filePath, QueryTypes.SELECT, tableName);
+                if(!Main.isTransaction)
+                Results.printResult(queryResult);
             }
         }
+        return operationStatus;
     }
 
     private ArrayList filterProjectionsForOutput(List<Map> resultList, ArrayList projectionList) {
